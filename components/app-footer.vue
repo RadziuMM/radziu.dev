cceb4adb6ebae83f169112b15f74febcf9cd211f
Radek <RMazurczak@protonmail.com>
2023-03-30 22:13:18 +0200

    feat: update

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -10,9 +10,9 @@
 
 <style lang="scss">
  .AppFooter {
-  padding: 0.2rem;
-  text-align: center;
-  color: rgba(var(--color-base));
-  background-color: rgba(var(--color-primary), 0.8);
+    padding: 0.2rem;
+    text-align: center;
+    color: rgba(var(--color-base));
+    background-color: rgba(var(--color-primary), 0.8);
  }
 </style>
